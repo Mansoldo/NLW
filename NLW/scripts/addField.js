ecb5ab9e30a571c1d5492686e9ed67b893b0a8d7
Mansoldo <dmansoldo@gmail.com>
2020-08-06 13:51:08 -0300

    Início do JS

---
+++ NLW/scripts/addField.js
@@ -0,0 +1,21 @@
+// Procurar o botão
+document.querySelector("#add-time").addEventListener('click', cloneField);
+// Quando clicar no botão
+
+//Executar uma ação
+function cloneField(){
+    //Duplicar os campos. Quais campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);   
+
+    //Pegar os campos? Quais campos?
+    const fields = newFieldContainer.querySelectorAll('input');
+
+    //Para cada campo, limpar
+    fields.array.forEach(function(field){
+        //Pegar o field do momento e limpa
+        field.value = ""
+    });
+    //Colocar na página. Onde?
+    document.querySelector('#schedule-items').appendChild(newFieldContainer);
+}
+    
